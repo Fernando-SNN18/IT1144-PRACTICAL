5b3515a8efc4a7afd6cfd5910adc08a884456d06
Fernando-SNN18 <fnanethmi02@gmail.com>
2026-06-07 01:17:40 +0530

    Add files via upload

---
+++ JAVA01.js
@@ -0,0 +1,14 @@
+console.log("HI NAVANJANA");
+
+let StudentName="NAVANJANA";
+let Age=21;
+let HomeTown="Chilaw";
+
+console.log(StudentName);
+console.log(Age);
+console.log(HomeTown);
+
+let score=70;
+console.log(score);
+score=90;
+console.log(score);
