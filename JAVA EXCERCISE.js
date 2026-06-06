1496134c0edf4bf24d51866cb420eb9273daaaa5
Fernando-SNN18 <fnanethmi02@gmail.com>
2026-06-07 01:46:48 +0530

    Add files via upload

---
+++ JAVA EXCERCISE.js
@@ -0,0 +1,19 @@
+let marks=Prompt("Enter your marks-")
+marks=Number(marks)
+
+if(marks>=75){
+	console.log("Grade A");
+}
+
+else if((marks>=65){
+	console.log("Grade B");
+}
+
+else if(marks>=55){
+	console.log("Grade C");
+}
+else if((marks>=35){
+	console.log("Grade S");
+}
+else{console.log("Fail");
+}
