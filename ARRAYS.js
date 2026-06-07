0c2425e1107c82236e8d95d144b49179ef6b59a2
Fernando-SNN18 <fnanethmi02@gmail.com>
2026-06-07 08:12:40 +0530

    Add files via upload

---
+++ ARRAYS.js
@@ -0,0 +1,21 @@
+const family=["Madhan ❤️","Jani ❤️","Dhanu ❤️","Akash ❤️","Chamo ❤️","Dini ❤️","Nethu ❤️","Daham ❤️","Yasu ❤️","Dewmi ❤️","Dasu ❤️","Himash ❤️","Nisu ❤️","Yumeth❤️"];
+
+console.log("Team Bride:");
+console.log(family[0]);
+console.log(family[1]);
+console.log(family[2]);
+console.log(family[3]);
+console.log(family[4]);
+console.log(family[5]);
+console.log(family[6]);
+console.log(family[7]);
+console.log(family[8]);
+console.log(family[9]);
+console.log(family[10]);
+console.log(family[11]);
+console.log(family[12]);
+console.log(family[13]);
+
+
+
+
